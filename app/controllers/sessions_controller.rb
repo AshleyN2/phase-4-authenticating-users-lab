d7a0715ffb497bfb4ce513bc193d231d15668fce
Ashley <ashleynjeri7@gmail.com>
2022-12-07 23:24:37 +0300

    Sessions

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,13 @@
+class SessionsController < ApplicationController
+    def create
+        user = User.find_by(username: params[:username])
+        render json: user
+        session[:user_id] = user.id
+    end
+
+    def destroy
+        head :no_content
+        session.delete :user_id
+    end
+
+end
